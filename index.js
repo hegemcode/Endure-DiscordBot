43b4abe384b734b49ce1220d3a7867a3aa7bbe8f
Pedro <hegemcodes@gmail.com>
2019-08-10 13:35:11 +0200

    Small Commit

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 //Call package and define discord client
 const Discord = require("discord.js");
 const client = new Discord.Client();
-//Call all your personal/secret information
+const Google = require('./commands/google')
+
+
+//Personal/secret information
 const secretKeys = require('./superSecretKeys.json');
 
-//Moderator role you need to run certain commands. Enter your server's mod role
-const modRole = "mod";
 
-//Events when the BOT is ready
+//Event when the BOT is ready
 client.on("ready", () => {
   console.log(`Bot is ready as: ${client.user.tag}`);
-  //Change status of the BOT: iddle, [iddle, invisible, online, dnd]
+  //Change status of the BOT: [iddle, invisible, online, dnd]
   client.user.setStatus("online");
   client.user.setActivity("type .help", { type: "LISTENING" });
   //Current BOT status
   console.log(`Status: ${client.user.presence.status.toUpperCase()}`);
 });
 
+
 //Events when a message is received
 client.on("message", async message => {
   //Receiveing the message
   console.log(`${message.author.tag}: ${message.content} `);
-
   //Variables
   let prefix = ".";
+  let modRole = "mod"; //Moderator role you need to run certain commands. Enter your server's mod role
   let msg = message.content.slice(prefix.length).split(" "); //Slices off the prefix
   let args = msg.slice(1); //Message after removing the command part
   const globalEmoji = message.guild.emojis.get('602754789910118400'); //Global emoji used to react to the user commands, change its ID to your emoji's one.
-
-  //Commands
-
+  
   //Funny command
   if (message.content.includes("awo")) {
     const attachment = new Discord.Attachment(
@@ -38,6 +38,7 @@ client.on("message", async message => {
     message.channel.send(`Awoooo :3 ${message.author}!`, attachment);
     message.react(globalEmoji); //React to the command message
   }
+
 
   //Link to an anime web
   if (message.content === `${prefix}anime`) {
@@ -55,6 +56,12 @@ client.on("message", async message => {
   }
 
 
+  //Command to google whatever you want
+  if(Google.match(message)){
+    Google.parse(message)
+  }
+
+
   //Display general information about the bot and all the commands
   if (message.content === `${prefix}help`) {
     const embed = new Discord.RichEmbed() //Display a pretty looking message
@@ -64,10 +71,11 @@ client.on("message", async message => {
         "https://i.gyazo.com/355c692ae81c742278cfae8d74989778.png"
       )
       .addField("About Me", "Hi there, I'm  the first bot developed by pedrohegem.")
-      .addField("Commands", ".anime, .clear, .clear all,  .help, .waduhek");
+      .addField("Commands", ".anime, .clear, .clear all, .google, .help, .waduhek");
     message.channel.send(embed);
     message.delete();
   }
+
 
   //Remove the last 'X' messages
    if (message.content.startsWith(`${prefix}clear`)) {
@@ -83,6 +91,7 @@ client.on("message", async message => {
       message.channel.send(`You lack of permissions to execute this command ${message.author}`);
     }
   } 
+
 
   //Clear 'all' the messages. Powerfull command
   if (message.content === `${prefix}clear all`){
@@ -102,3 +111,4 @@ client.on("message", async message => {
 
 //BOT logs in. Enter your bot's token here.
 client.login(secretKeys.token);
+
